23906dc7effd0be6c89f079cc47a71731c0b855b
zxc5356061 <96610052+zxc5356061@users.noreply.github.com>
2022-05-06 18:43:15 +0800

    Add files via upload

--- SampleTS.ts
+++ SampleTS.ts
@@ -1,6 +1,12 @@
 describe("Register form", ()=>{
     it("enter some value", ()=>{
+        //Visit Login Portal
         cy.visit("https://ifp-organizer-ifpsdemo-eks005.sa.wise-paas.com/#/");
+
+        //Verify address
+        cy.url().should("include", "/signIn");
+        
+        //Login
         cy.get("input[placeholder='Username']").type("devanliang@iii.org.tw");
         cy.get("input[placeholder='Password']").type("Abcd1234#");
         cy.get(".jss55").click();
