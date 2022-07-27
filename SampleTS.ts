21af95f8d1663aaaa178fad8f1e59b0810475fed
zxc5356061 <96610052+zxc5356061@users.noreply.github.com>
2022-07-27 16:29:42 +0800

    Update SampleTS.ts

--- SampleTS.ts
+++ SampleTS.ts
@@ -1,14 +1,20 @@
-describe("Register form", ()=>{
-    it("enter some value", ()=>{
-        //Visit Login Portal
-        cy.visit("https://ifp-organizer-ifpsdemo-eks005.sa.wise-paas.com/#/");
+describe('LoginLogout', () => {
+  it('passes', () => {
+    //Clean cookies and local storage
+    cy.clearLocalStorage().clearCookies
+    
+    //Login
+    cy.visit('https://ifp-organizer-ifpsdemo-eks005.sa.wise-paas.com/#/signIn')
+    cy.get('form').get('input[placeholder="Username"]').type('devanliang@iii.org.tw')
+    cy.get('form').get('input[placeholder="Password"]').type('Abcd1234#')
+    cy.get('form').get('button[type="submit"]').click()
 
-        //Verify address
-        cy.url().should("include", "/signIn");
-        
-        //Login
-        cy.get("input[placeholder='Username']").type("devanliang@iii.org.tw");
-        cy.get("input[placeholder='Password']").type("Abcd1234#");
-        cy.get(".jss55").click();
-    })
+    //Check Account Name
+    cy.get('.MuiTypography-root.jss106.jss108.MuiTypography-body1').click()
+    cy.get('.MuiTypography-root.jss91').contains('devanliang@iii.org.tw')
+
+    //Logout
+    cy.get('.MuiTypography-root.jss106.jss108.MuiTypography-body1').get('div[id="user-menu"] li[role="menuitem"]').click()
+    cy.url().should('include','signIn');//confirm whether logout successfully
+  })
 })
